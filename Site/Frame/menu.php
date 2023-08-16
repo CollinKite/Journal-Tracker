04ea17a03f4d45dd00c9e2eb0d861f983df10f06
choffman4 <choffman@student.neumont.edu>
2023-08-16 08:28:31 -0600

    Update menu.php

--- Site/Frame/menu.php
+++ Site/Frame/menu.php
@@ -1,8 +1,53 @@
+<Span>
+    <Span class="menu-button">Home</Span>
+    <Span class="menu-button">Create Journal</Span>
+    <Span class="menu-button">Search Journal</Span>
+    <div id="dropdownProfileContainer" class="dropdown-profile-container">
+        <a href="profile.php">My Profile</a>
+        <ul>
+            <li><a href="account.php">Account</a></li>
+            <li><a href="settings.php">Settings</a></li>
+            <li><a href="logout.php">Logout</a></li>
+        </ul>
+    </div>
+</Span>
+
+<style>
+    .dropdown-profile-container {
+        position: relative;
+        display: inline-block;
+    }
+
+    .dropdown-profile-container > a {
+        display: block;
+        padding: 10px;
+        text-decoration: none;
+        color: #333;
+    }
+
+    .dropdown-profile-container ul {
+        position: absolute;
+        top: 100%;
+        left: 0;
+        display: none;
+        background-color: #f9f9f9;
+        border: 1px solid #ccc;
+        list-style: none;
+        padding: 0;
+        margin: 0;
+    }
+
+    .dropdown-profile-container:hover ul {
+        display: block;
+    }
+
+    .dropdown-profile-container li {
+        padding: 8px 15px;
+    }
+
+    .dropdown-profile-container li:hover {
+        background-color: #e0e0e0;
+    }
 
 
-<div id="dropdownProfileContainer" class="dropdown-profile-container">
-    <Span>Home</Span>
-    <Span>Create Journal</Span>
-    <Span>Search Journal</Span>
-    <Span>Profile</Span>
-</div>
+</style>
